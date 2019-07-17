730630a4146f2bef3024fb7cf9152d87a61ab944
Clément MOINE <clement.moine@ubidreams.com>
2019-07-17 14:38:44 +0200

    Scripting : Character Collision 2D

--- Assets/Scripts/SnowmanController.cs
+++ Assets/Scripts/SnowmanController.cs
@@ -6,12 +6,34 @@ public class SnowmanController : MonoBehaviour
 {
     public Animator animator;
 
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
     // Update is called once per frame
     void Update()
     {
-        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal * Time.deltaTime;
+        // Always positive speed movement
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        //if (Input.GetButtonDown("Jump"))
+        //{
+        //    jump = true;
+        //}
+
+        //if (Input.GetButtonDown("Crouch"))
+        //{
+        //    crouch = true;
+        //} else if (Input.GetButtonUp("Crouch"))
+        //{
+        //    crouch = false;
+        //}
+
+        //Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        //transform.position = transform.position + horizontal * Time.deltaTime;
     }
 }
